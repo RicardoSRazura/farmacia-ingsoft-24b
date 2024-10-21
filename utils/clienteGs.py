106ce5ec9b2d01cfd558a9d2308f5b97d5201d21
Ricardo Razura <ricardorazest@gmail.com>
2024-10-20 18:42:46 -0700

    Ricardo: Merge

--- utils/clienteGs.py
+++ utils/clienteGs.py
@@ -7,25 +7,24 @@ class Cliente:
 
         def getIdCliente(self):
             return self.id_cliente
-        
+
         def setIdCliente(self, id_cliente):
             self.id_cliente = id_cliente
 
         def getNombre(self):
             return self.nombre
-        
+
         def setNombre(self, nombre):
             self.nombre = nombre
 
         def getRfc(self):
             return self.rfc
-        
+
         def setRfc(self, rfc):
             self.rfc = rfc
 
         def getPuntosAcumulados(self):
             return self.puntos_acumulados
-        
+
         def setPuntosAcumulados(self, puntos_acumulados):
             self.puntos_acumulados = puntos_acumulados
-        
